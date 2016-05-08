ed6b4f912df255a50100d7d2450a6c264dd05ab5
Nick Millward <nicholasmillward@gmail.com>
2016-05-07 20:50:12 -0400

    created main view

--- app/src/main/java/com/nickmillward/hackweekspacegame/MainActivity.java
+++ app/src/main/java/com/nickmillward/hackweekspacegame/MainActivity.java
@@ -3,11 +3,33 @@ package com.nickmillward.hackweekspacegame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nickmillward.hackweekspacegame.view.SpaceAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SpaceAnimationView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (SpaceAnimationView) findViewById(R.id.view_spaceAnimationView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (view != null) {
+            //resume SpaceAnimationView
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (view != null) {
+            //pause SpaceAnimationView
+        }
     }
 }
---
+++ app/src/main/java/com/nickmillward/hackweekspacegame/view/SpaceAnimationView.java
@@ -0,0 +1,25 @@
+package com.nickmillward.hackweekspacegame.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by nmillward on 5/7/16.
+ */
+public class SpaceAnimationView extends FrameLayout {
+    public SpaceAnimationView(Context context) {
+        super(context);
+    }
+
+    public SpaceAnimationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SpaceAnimationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
