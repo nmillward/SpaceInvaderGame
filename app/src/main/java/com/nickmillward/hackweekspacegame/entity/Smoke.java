66eea5e1bf755112e7982537d9b7464d7360a2b7
Nick Millward <nicholasmillward@gmail.com>
2016-05-10 23:38:33 -0400

    create smoke object

---
+++ app/src/main/java/com/nickmillward/hackweekspacegame/entity/Smoke.java
@@ -0,0 +1,45 @@
+package com.nickmillward.hackweekspacegame.entity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+
+/**
+ * Created by nmillward on 5/10/16.
+ * Hackweek - May 2016
+ */
+public class Smoke {
+
+    public float x, y;
+    public float diameter;
+    public float rotation;
+
+    private Bitmap smokeBitmap;
+    private Matrix smokeMatrix = new Matrix();
+    private Paint smokePaint;
+
+    public Smoke(float diameter) {
+        this.diameter = diameter;
+        smokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        if (smokeBitmap == null) {
+            createSmokeBitmap();
+        } else if (diameter != smokeBitmap.getWidth()) {
+            createSmokeBitmap();
+        }
+    }
+
+    private void createSmokeBitmap() {
+        smokeBitmap = Bitmap.createBitmap((int) diameter, (int) diameter, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(smokeBitmap);
+        canvas.drawColor(0x80000000);     //50% Black
+    }
+
+    public void drawSmoke(Canvas canvas) {
+        smokeMatrix.reset();
+        smokeMatrix.setTranslate(x, y);
+        smokeMatrix.postRotate(rotation, x + diameter / 2, y + diameter / 2);
+        smokePaint.setAlpha((int) (0xff * (1.f - rotation/400.f)));
+        canvas.drawBitmap(smokeBitmap, smokeMatrix, smokePaint);
+    }
+}
